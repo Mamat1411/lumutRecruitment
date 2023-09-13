943ca11cea3d63003842a76ba5c9a6c87d7b248e
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-13 14:59:50 +0700

    Relationship Implementation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -19,4 +20,8 @@ class User extends Authenticatable
      */
     protected $guarded = ['id'];
     public $timestamps = false;
+
+    public function posts() : HasMany {
+        return $this->hasMany(Post::class);
+    }
 }
