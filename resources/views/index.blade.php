d3d236631c1d0cf9eb5cdf7c94efc094387925df
Mamat1411 <muhammadmujahid14@gmail.com>
2023-09-13 15:13:11 +0700

    Add author name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,6 +15,7 @@
               <tr>
                 <th scope="col">No</th>
                 <th scope="col">Action</th>
+                <th scope="col">Author</th>
                 <th scope="col">Title</th>
                 <th scope="col">Content</th>
               </tr>
@@ -22,18 +23,19 @@
             <tbody>
                 @foreach ($posts as $p)
                     <tr>
-                    <th scope="row">{{ $p->id }}</th>
-                    <td>
-                       <a href="/edit/{{ $p->id }}" class="badge text-bg-success text-decoration-none">Edit</a>
-                       <a href="/show/{{ $p->id }}" class="badge text-bg-primary text-decoration-none">Detail</a>
-                       <form action="/destroy/{{ $p->id }}" method="post" class="d-inline">
-                            @method('delete')
-                            @csrf
-                           <button type="submit" class="badge text-bg-danger text-decoration-none">Delete</button>
-                       </form>
-                    </td>
-                    <td>{{ $p->title }}</td>
-                    <td>{{ $p->content }}</td>
+                        <th scope="row">{{ $p->id }}</th>
+                        <td>
+                        <a href="/edit/{{ $p->id }}" class="badge text-bg-success text-decoration-none">Edit</a>
+                        <a href="/show/{{ $p->id }}" class="badge text-bg-primary text-decoration-none">Detail</a>
+                        <form action="/destroy/{{ $p->id }}" method="post" class="d-inline">
+                                @method('delete')
+                                @csrf
+                            <button type="submit" class="badge text-bg-danger text-decoration-none">Delete</button>
+                        </form>
+                        </td>
+                        <td>{{ $p->author->name }}</td>
+                        <td>{{ $p->title }}</td>
+                        <td>{{ $p->content }}</td>
                     </tr>
                 @endforeach
             </tbody>
